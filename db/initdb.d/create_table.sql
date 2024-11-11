801d483ddc11823033460611e5afe3acc388263e
hahahellooo <hahahello777@gmail.com>
2024-11-11 12:46:18 +0900

    done

--- db/initdb.d/create_table.sql
+++ db/initdb.d/create_table.sql
@@ -1,7 +1,5 @@
-CREATE TABLE ticket_data(
-	`event_name` VARCHAR(50) NOT NULL,
-	`price` INT NOT NULL,		
-	`date` VARCHAR(25) NOT NULL,
-	`location` VARCHAR(50) NOT NULL,
-	`available_tickets` VARCHAR(25)	NOT NULL
-);
+CREATE TABLE ticket_data (event_name VARCHAR(50) NOT NULL COMMENT '공연이름',
+price VARCHAR(50) NOT NULL COMMENT '가격',
+date VARCHAR(100) NOT NULL COMMENT '날짜',
+location VARCHAR(100) NOT NULL COMMENT '장소',
+available_tickets VARCHAR(100) NOT NULL COMMENT '남은티켓');

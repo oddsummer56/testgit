9923e2dca9e6ec3f0ec2d350d13f872a498e7e31
hahahellooo <hahahello777@gmail.com>
2024-11-11 12:45:06 +0900

    table 생성

---
+++ db/initdb.d/create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE ticket_data(
+	`event_name` VARCHAR(50) NOT NULL,
+	`price` INT NOT NULL,		
+	`date` VARCHAR(25) NOT NULL,
+	`location` VARCHAR(50) NOT NULL,
+	`available_tickets` VARCHAR(25)	NOT NULL
+);

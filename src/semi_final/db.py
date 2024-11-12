2e872b9c4f0cbf8b0acc370311e16ff2223253a6
sunwoo <summerham22@gmail.com>
2024-11-12 12:06:33 +0900

    ID와 비밀번호 가림

--- src/semi_final/db.py
+++ src/semi_final/db.py
@@ -5,8 +5,8 @@ def get_conn():
     conn = pymysql.connect(
                             host="13.124.42.108",            # 컨테이너 이름을 호스트로 사용
                             port=6033,
-                            user="tut",
-                            password="tut1234",
+                            user=os.getenv('DB_USER'),
+                            password=os.getenv('DB_PASSWORD'),
                             database="ticket",
                             cursorclass=pymysql.cursors.DictCursor)
     return conn

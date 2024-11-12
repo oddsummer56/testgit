ef3b4131834285355f5c95d27a7647873715d01a
sunwoo <summerham22@gmail.com>
2024-11-12 12:02:51 +0900

    db연결 성공

--- src/semi_final/db.py
+++ src/semi_final/db.py
@@ -3,8 +3,8 @@ import os
 
 def get_conn():
     conn = pymysql.connect(
-                            host="mariadb",            # 컨테이너 이름을 호스트로 사용
-                            port=3306,
+                            host="13.124.42.108",            # 컨테이너 이름을 호스트로 사용
+                            port=6033,
                             user="tut",
                             password="tut1234",
                             database="ticket",
